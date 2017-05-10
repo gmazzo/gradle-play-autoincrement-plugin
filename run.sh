bf8b7752c9d829186630c6d9c11c7f8b5d96044a
guillermo.mazzola <guillermo.mazzola@globant.com>
2017-05-10 16:21:49 -0300

    Removed PLUGIN_DEPENDENCY property

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-./gradlew install && ./gradlew -p plugin/src/test/resources/project/ assembleRelease -DPLUGIN_DEPENDENCY=gradle.plugin.gmazzo:play-autoincrement-plugin:+ "$@"
+./gradlew install && ./gradlew -p plugin/src/test/resources/project/ assembleRelease "$@"
